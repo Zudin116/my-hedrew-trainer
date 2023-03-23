d348bb273049812d5f2c04a17dc2360d1e85dbdb
Valentin Basin <zudin116@gmail.com>
2023-03-23 19:54:14 +0200

    version 0.0.1

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from aiogram import executor
 
 from create_bot import dp
-from handlers import client, other
+from handlers import client
 
 
 async def on_startup(_):
@@ -9,6 +9,5 @@ async def on_startup(_):
 
 
 client.register_handlers_client(dp)
-other.register_handlers_other(dp)
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
--- handlers/client.py
+++ handlers/client.py
@@ -15,7 +15,13 @@ class FSMQuiz(StatesGroup):
 async def send_welcome(message: types.Message):
     await bot.send_message(
         message.from_user.id,
-        "Hello!\nI'm EchoBot!\nPowered by aiogram.",
+        """Hello!
+        I'm Hedrew numbers bot!
+        If you enter a number i will convert it to words(0-10000).
+        
+        /help, /start - Show this message.
+        /quiz - litle quiz about hedrew numbers.
+        """,
         reply_markup=kb_client,
     )
 
@@ -31,7 +37,8 @@ async def convertor(message: types.Message):
 
 async def quiz(message: types.Message, state: FSMContext):
     number = random.randrange(10)
-    await message.answer(num2words(number, lang="he"))
+
+    await message.answer("What number is written here: " + num2words(number, lang="he"))
     await state.set_state()
     async with state.proxy() as data:
         data["number"] = number
@@ -40,17 +47,25 @@ async def quiz(message: types.Message, state: FSMContext):
 
 async def check_answer(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
-        data["answer"] = int(message.text)
-        if data["number"] == data["answer"]:
-            await message.answer("True")
+        if data["number"] == int(message.text):
+            await message.answer("You're God damn right")
             await state.finish()
         else:
-            await message.answer("False")
+            await message.answer("Try again: " + num2words(data["number"], lang="he"))
             await FSMQuiz.answer.set()
+
+
+async def convertor(message: types.Message):
+    print(message)
+    try:
+        number = int(message.text)
+    except:
+        await message.answer("Enter a number")
+    await message.answer(num2words(number, lang="he"))
 
 
 def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(send_welcome, commands=["start", "help"])
     dp.register_message_handler(quiz, commands=["quiz"], state=None)
     dp.register_message_handler(check_answer, state=FSMQuiz.answer)
-    # dp.register_message_handler(convertor)
+    dp.register_message_handler(convertor, lambda message: message.text.isdigit())
